3fcc5f5b7a5e400d5fd7fe947a01c65b2b21868c
nakamoriryota <ryota.nakamori@gmail.com>
2021-08-22 20:09:38 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dot=(document.querySelectorAll('.item'))
+    dot[160].style.background='red'
+    dot[170].style.background='red'
+    dot[180].style.background='red'
+    dot[181].style.background='red'
+  });
